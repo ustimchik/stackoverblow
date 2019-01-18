e523219b72fe92548a09eb332088b7eac84baf36
ustimchik <alexander.ustimchik@gmail.com>
2019-01-18 13:43:22 +0000

    answers depend on questions with specs

--- db/migrate/20190111013103_add_question_id_to_answers.rb
+++ db/migrate/20190111013103_add_question_id_to_answers.rb
@@ -1,5 +1,5 @@
 class AddQuestionIdToAnswers < ActiveRecord::Migration[5.2]
   def change
-    add_belongs_to :answers, :question
+    add_belongs_to :answers, :question, foreign_key: true
   end
 end
