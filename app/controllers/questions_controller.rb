81ae4f1d4ecba97f04e936659a77519e5b22a147
ustimchik <alexander.ustimchik@gmail.com>
2019-01-23 20:11:14 +0000

    questions_controllers new-edit with specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,28 @@
 class QuestionsController < ApplicationController
 
+  before_action :set_question, only: [:show, :edit]
+
   def index
     @questions = Question.all
   end
 
   def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_question
     @question = Question.find(params[:id])
   end
 
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let (:question_test) { create(:question) }
 
   describe 'GET #index' do
     let(:questions) { create_list(:question, 4) }
@@ -17,16 +18,39 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let (:question_test) { create(:question) }
 
     before { get :show, params: {id: question_test} }
 
-    it 'loads the resource with specified id from params' do
+    it 'assigns question with id from params to @question' do
       expect(assigns(:question)).to eq question_test
     end
 
     it 'renders show view' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'creates a new question and assigns to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+
+    before { get :edit, params: {id: question_test} }
+
+    it 'assigns question with id from params to @question' do
+      expect(assigns(:question)).to eq question_test
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
     end
   end
 
