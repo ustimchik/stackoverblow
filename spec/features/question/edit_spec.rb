2a63dd77ffda0179eca3f04f82997e03213b4ca2
Alexander Ustimchik <alexander.ustimchik@gmail.com>
2019-04-02 15:27:19 -0400

    attach multiple files to question when editing

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -8,7 +8,7 @@ feature 'User can edit question', %q{
 
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given(:other_user) { create(:user) }
+  given(:other_question) { create(:question) }
 
   scenario 'Unauthenticated user can not edit question' do
     visit questions_path(question)
@@ -17,25 +17,24 @@ feature 'User can edit question', %q{
   end
 
   describe 'Authenticated user' do
-    scenario 'edits his question', js: true do
+    before do
       sign_in user
       visit question_path(question)
-      click_on 'Edit'
+    end
 
+    scenario 'edits his question', js: true do
       within '.question' do
+        click_on 'Edit'
         fill_in 'Body', with: 'edited question'
         click_on 'Save'
 
         expect(page).to_not have_content question.body
         expect(page).to have_content 'edited question'
         expect(page).to_not have_selector 'textarea'
-
       end
     end
-    scenario 'edits his question with errors', js: true do
-      sign_in user
-      visit question_path(question)
 
+    scenario 'edits his question with errors', js: true do
       within '.question' do
         click_on 'Edit'
         fill_in 'Body', with: ''
@@ -47,11 +46,22 @@ feature 'User can edit question', %q{
         expect(page).to have_content "can't be blank"
       end
     end
+
     scenario "does not have option to edit question of another user" do
-      sign_in other_user
-      visit questions_path(question)
+      visit questions_path(other_question)
 
       expect(page).to_not have_link 'Edit'
+    end
+
+    scenario 'edits his question with no errors and with attached files', js: true do
+      within '.question' do
+        click_on 'Edit'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+      end
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 end
