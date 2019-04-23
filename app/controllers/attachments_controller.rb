3029a5553b6acfdfff2c1cfc5b2018a2b8a71f81
Alexander Ustimchik <alexander.ustimchik@gmail.com>
2019-04-23 09:54:50 -0400

    remove only attachment by destroy js

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,6 @@ class AttachmentsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    @question = Question.find(params[:question])
     @attachment = ActiveStorage::Attachment.find(params[:id])
     @attachment.purge_later if current_user.author_of?(@attachment.record)
   end
