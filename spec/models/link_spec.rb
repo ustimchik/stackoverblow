281eac8ae53a7b28f5be8aea8da6767143053d4f
Alexander Ustimchik <alexander.ustimchik@gmail.com>
2019-04-28 12:13:57 -0400

    added URL validation with specs

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Link, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
 
+  describe 'validates url format for link URL' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+
+    it 'with wrong input' do
+      question.links.new({'name':'testname', 'url': 'random string'})
+      expect(question.links.first).to be_invalid
+    end
+
+    it 'with correct input' do
+      question.links.new({'name':'testname', 'url': 'https://google.com'})
+      expect(question.links.first).to be_valid
+    end
+  end
 end
